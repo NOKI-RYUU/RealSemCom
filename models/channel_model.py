833d21b7fe33ac3cbe7fa1a024050d701d0ec30b
NOKI RYUU <117287514+NOKI-RYUU@users.noreply.github.com>
2025-03-12 16:32:30 +0800

    Update channel_model.py

--- models/channel_model.py
+++ models/channel_model.py
@@ -1,13 +1,19 @@
 import torch
 import torch.nn as nn
+import numpy as np
 
 
 class ChannelModel(nn.Module):
-    """语义信道模型"""
-    def __init__(self, noise_std=0.1):
+    """信道模型"""
+    def __init__(self, noise_std=0.1, fading=True):
         super().__init__()
         self.noise_std = noise_std
+        self.fading = fading
 
     def forward(self, x):
-        noise = torch.randn_like(x) * self.noise_std
-        return x + noise  # 简单 AWGN 信道
+        if self.fading:
+            h = torch.randn_like(x) * np.sqrt(0.5) + 1j * torch.randn_like(x) * np.sqrt(0.5)  # Rayleigh 信道
+            x = x * torch.abs(h)  # 乘以信道增益
+
+        noise = torch.randn_like(x) * self.noise_std  # AWGN 噪声
+        return x + noise
